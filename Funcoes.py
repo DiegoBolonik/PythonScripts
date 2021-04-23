1602878c8d8d241cacb4693e22b59a54e10003c5
Diego De Morais <79227339+DiegoBolonik@users.noreply.github.com>
2021-04-23 16:06:33 -0300

    Update and rename 6.Funcoes.ipynb to Funcoes.py

---
+++ Funcoes.py
@@ -0,0 +1,42 @@
+#Funções
+
+def imprime():
+
+    print("esta é uma função")
+
+​
+
+#imprime()
+#com parametro
+
+def imprime(n):
+
+    print(n)
+
+​
+
+#imprime("Impressão deste texto")
+#com retorno
+
+def potencia(n):
+
+    return n * n
+
+​
+
+x = potencia(3)
+
+print(x)
+
+#com valor default
+
+def intervalo(inic=1,fim=10):
+
+    for n in range(inic, fim+1):
+
+        print(n)
+
+​
+
+x = intervalo(1,10)
+y = intervalo()
