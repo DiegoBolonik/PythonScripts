f30e345f3d6d7384731c445ef235cfea34f08c9c
Diego De Morais <79227339+DiegoBolonik@users.noreply.github.com>
2021-04-23 16:03:41 -0300

    Update and rename 5.ModulosPacotes.ipynb to ModulosPacotes.py

---
+++ ModulosPacotes.py
@@ -0,0 +1,45 @@
+#Módulos e Pacotes
+
+import statistics 
+
+z = [10,20,30,40]
+
+x = statistics.mean(z)
+
+y = statistics.median(z)
+
+print(x)
+print(y)
+
+import statistics  as est
+
+z = [10,20,30,40]
+
+x = est.mean(z)
+
+y = est.median(z)
+
+print(x)
+print(y)
+
+from statistics import mean, median
+
+z = [10,20,30,40]
+
+x = mean(z)
+
+y = median(z)
+
+print(x)
+print(y)
+
+from statistics import *
+
+z = [10,20,30,40]
+
+x = mean(z)
+
+y = median(z)
+
+print(x)
+print(y)
